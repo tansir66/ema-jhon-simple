ccd26dff1dfcf676eb3aa4f9811ce2035fea42ee
Tansir Afridi <tansir33@gmail.com>
2023-03-27 01:52:50 +0600

    function added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
+    
     const { img, name, seller, ratings, price } = props.product;
+
+    const handleAddToCart = props.handleAddToCart;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -11,7 +14,7 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Ratings: {ratings} stars </p>
             </div>
-            <button className='add-cart'>Add to Cart</button>
+            <button  onClick={() => handleAddToCart(props.product)} className='add-cart'>Add to Cart</button>
         </div>
     );
 };
