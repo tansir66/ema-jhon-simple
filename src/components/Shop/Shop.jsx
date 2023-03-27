5d19102e90119cfd9cff7faa9d21cadb4aef4b9d
Tansir Afridi <tansir33@gmail.com>
2023-03-28 01:20:32 +0600

    local storage part complete

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -40,7 +40,24 @@ const Shop = () => {
 
     const handleAddToCart = (product) =>{
         // cart.push(product);
-        const newCart = [...cart, product];
+        // const newCart = [...cart, product];
+
+        let newCart = [];
+
+        //if product does not exist in the cart then set quantity 1;
+        //if exist then updated quantity by 1;
+        let exists = cart.find(pd=> pd.id===product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining,  exists];
+        }
+
+
         setCart(newCart);
         addToDb(product.id);
     }
